99d42291cd7a19bf47fca4d667dd32d5805d7801
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2024-09-29 13:58:30 +0530

    learning about routes

--- src/app.js
+++ src/app.js
@@ -2,10 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res) => {
-  res.send("server is running good good");
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log(req.query);
+    if (req.query.name == "abhi" || req.query.id == 5) {
+      next();
+    } else {
+      res.send("route handler 1");
+    }
+  },
+  (req, res) => {
+    console.log("user 2");
+    //next();
+    res.send("route handler 2");
+  }
+);
 
 app.listen(3000, () => {
-  console.log("server is running at port 3000");
+  console.log("app is running on port number 3000");
 });
