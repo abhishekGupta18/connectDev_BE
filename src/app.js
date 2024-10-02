4b457f0031f425e985686993b581ec2ec430e4ef
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2024-10-02 07:55:11 +0530

    made connection with database

--- src/app.js
+++ src/app.js
@@ -1,23 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/database");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
-
-app.use("/admin", adminAuth);
-
-app.use("/user/getData", userAuth, (req, res) => {
-  res.send("all user data send");
-});
-
-app.get("/admin/getData", (req, res) => {
-  res.send("all admin data send");
-});
-
-app.get("/admin/deleteData", (req, res) => {
-  res.send("admin data deleted");
-});
-
-app.listen(3000, () => {
-  console.log("app is running on port number 3000");
-});
+connectDB()
+  .then(() => {
+    console.log("database connection established...");
+    app.listen(3000, () => {
+      console.log("app is running on port number 3000");
+    });
+  })
+  .catch((e) => {
+    console.log("databse connection failed!!", e);
+  });
---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  await mongoose.connect(
+    "mongodb+srv://abhishekg12703:lfKPAYfhxGaXdjbk@cluster0.urnqx.mongodb.net/devTinder"
+  );
+};
+
+module.exports = connectDB;
+
+// "mongodb+srv://abhishekg12703:lfKPAYfhxGaXdjbk@cluster0.urnqx.mongodb.net/"
