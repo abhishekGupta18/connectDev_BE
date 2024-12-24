3235c0ab84fb5a61a3e2e0712eae165c8678c2cc
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2024-12-24 17:15:53 +0530

    completed user feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const userRouter = express.Router();
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
+const User = require("../models/user");
 
 const dataToBeSend = ["firstName", "lastName", "photoUrl", "about", "skills"];
 
@@ -61,6 +62,47 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     res.json({
       data,
     });
+  } catch (e) {
+    res.status(400).send("ERROR: " + e);
+  }
+});
+
+// feed api
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        {
+          toUserId: loggedInUser._id,
+        },
+        {
+          fromUserId: loggedInUser._id,
+        },
+      ],
+    }).select("fromUserId toUserId");
+
+    const hideUserFromFeed = new Set();
+
+    connectionRequests.forEach((req) => {
+      hideUserFromFeed.add(req.fromUserId);
+      hideUserFromFeed.add(req.toUserId);
+    });
+
+    const userFeed = await User.find({
+      $and: [
+        {
+          _id: { $nin: Array.from(hideUserFromFeed) },
+        },
+        {
+          _id: { $ne: loggedInUser._id },
+        },
+      ],
+    }).select(dataToBeSend); // have to implement pagination using skip() and limit() functions
+
+    res.send(userFeed);
   } catch (e) {
     res.status(400).send("ERROR: " + e);
   }
