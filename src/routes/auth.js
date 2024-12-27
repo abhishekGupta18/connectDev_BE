c0dd03dcd87f0d8cf8e1f3a51bc510fceae61865
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2024-12-28 00:03:33 +0530

    added cors middleware

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -65,7 +65,7 @@ authRouter.post("/login", async (req, res) => {
         expires: new Date(Date.now() + 8 * 3600000),
       });
 
-      res.send("login successfully");
+      res.send(user);
     } else {
       throw new Error("wrong credentials");
     }
