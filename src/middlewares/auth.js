6bc1af285d3be170897aceb288653787e60980cf
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-01-02 22:59:25 +0530

    made minor changes in auth middleware

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -5,7 +5,7 @@ const userAuth = async (req, res, next) => {
     const { token } = req.cookies;
 
     if (!token) {
-      throw new Error("invalid token !!");
+      return res.status(401).send("please login");
     }
     const decodedData = await jwt.verify(token, "abhi12703");
     const { _id } = decodedData;
