f786b403a32758c82251cb44990d1a6c9a3232c9
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-01-06 00:27:29 +0530

    fixed a bug issue in signup api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -29,16 +29,6 @@ authRouter.post("/signup", async (req, res) => {
       gender,
     });
 
-    // create token
-
-    const token = await user.getJwtToken();
-
-    // set cookie
-
-    res.cookie("token", token, {
-      expires: new Date(Date.now() + 8 * 3600000),
-    });
-
     await user.save();
     res.json({
       message: "user added successfully!!",
