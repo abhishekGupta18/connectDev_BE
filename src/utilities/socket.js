b33795787548960eaf3a85915bc30292488d73b3
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-02-27 18:17:15 +0530

    implemented crypto secret

--- src/utilities/socket.js
+++ src/utilities/socket.js
@@ -1,4 +1,13 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId) => {
+  return crypto
+    .createHash("sha256")
+    .update([userId, targetUserId].sort().join("-"))
+    .digest("hex");
+};
+
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
@@ -7,8 +16,18 @@ const initializeSocket = (server) => {
   });
 
   io.on("connection", (socket) => {
-    socket.on("joinChat", () => {});
-    socket.on("sendMessage", () => {});
+    socket.on("joinChat", ({ firstName, userId, targetUserId }) => {
+      // whenever chat happens it happens in a room and in that room  there are too many participants
+
+      const roomId = getSecretRoomId(userId, targetUserId);
+      console.log(firstName + " joined room " + roomId);
+      socket.join(roomId);
+    });
+    socket.on("sendMessage", ({ firstName, userId, targetUserId, text }) => {
+      const roomId = [userId, targetUserId].sort().join("-");
+      console.log(firstName + " " + text);
+      io.to(roomId).emit("messageRecevied", { firstName, text });
+    });
     socket.on("disconnect", () => {});
   });
 };
