834b7423040135b82caa4b3045fa2a0b4a1440ee
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-04-02 15:21:34 +0530

    made changes in socket.js

--- src/utilities/socket.js
+++ src/utilities/socket.js
@@ -12,7 +12,10 @@ const getSecretRoomId = (userId, targetUserId) => {
 const initializeSocket = (server) => {
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: [
+        "https://connectdev-community.vercel.app",
+        "http://localhost:5173",
+      ],
     },
   });
 
