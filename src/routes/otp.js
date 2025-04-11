09581a364180b0554e2eb30cf66c6cd6f2751731
Guptaabhii <112747867+Guptaabhii@users.noreply.github.com>
2025-04-11 23:38:45 +0530

    created verify-otp api

--- src/routes/otp.js
+++ src/routes/otp.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const OTP = require("../models/otp");
 const sendEmail = require("../utilities/sendEmail");
+const VerifiedEmail = require("../models/verifiedEmail");
 
 const otpRouter = express.Router();
 
 const generateOtp = () => Math.floor(100000 + Math.random() * 90000).toString();
 
+// send-otp api
 otpRouter.post("/send-otp", async (req, res) => {
   try {
     const { email } = req.body;
@@ -25,6 +27,30 @@ otpRouter.post("/send-otp", async (req, res) => {
     res.status(200).json({ message: "OTP sent successfully!" });
   } catch (e) {
     res.status(500).json({ error: "Failed to send OTP" });
+  }
+});
+
+// verify-otp api
+
+// take eamil and otp, check for existing otp, save email to verified email, deleteotp,
+
+otpRouter.post("/verify-otp", async (req, res) => {
+  try {
+    const { email, otp } = req.body;
+
+    const existingOtp = await OTP.findOne({ email, otp });
+
+    if (!existingOtp)
+      res.status(400).json({ error: "Invalid or expired OTP." });
+
+    await VerifiedEmail.create({ email });
+
+    await OTP.findByIdAndDelete({ _id: existingOtp._id });
+
+    res.status(200).json({ message: "OTP verified successfully!" });
+  } catch (e) {
+    console.error("Error verifying OTP:", e);
+    res.status(500).json({ error: "Internal server error" });
   }
 });
 
